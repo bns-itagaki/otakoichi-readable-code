1c582fd6eb144380e1d41d0550b3ac3b07f93ea5
otakoichi <115201010+otakoichi@users.noreply.github.com>
2022-11-02 12:24:58 +0900

    spec6: ファイル名main.pyへ変えたことによる実行方法の修正（readme.txt）及び、課題点をTODOコメントで記載

--- readme.txt
+++ readme.txt
@@ -3,4 +3,7 @@
 
 環境構築
     Windows_コマンドリスト.txtに従って、環境を構築し実行する。
-    
+
+実行方法
+    cd (環境のルート)/src
+    python main.py
--- src/main.py
+++ src/main.py
@@ -13,12 +13,12 @@ def main():
 
     # 実行時引数を取得
     args = sys.argv
-
+    
     # 表示するIDを取得
     dict_id = None
-    if len(args) > 0:
-        dict_id = args[0]
-
+    if len(args) > 1:
+        dict_id = args[1]
+    
     # 読み込んだ辞書データ格納用
     dic_data = dict()
 
@@ -27,13 +27,14 @@ def main():
         lines = f.readlines()
         for i, word in enumerate(lines):
             dic_data[i] = word.strip()
-            # print(f'{i}: {word.strip()}')
-
+            # TODO: wordをスペースでstripし、扱いやすい形式にする。
+    
     # 辞書の表示
     if dict_id is None:
-        pass
+        print(dic_data)
         # TODO: 全ての辞書を表示
     else:
+        print('else')
         pass
         # TODO: dict_idで指定されたものを表示
 
