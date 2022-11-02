2720c40ded4e2f30b05d510daa0d1be12528501b
otakoichi <115201010+otakoichi@users.noreply.github.com>
2022-11-02 11:52:11 +0900

    spec4: 辞書データファイルのパスを専用の定数変数を使用するように修正

--- readable_code.py
+++ readable_code.py
@@ -1,5 +1,14 @@
+import pathlib
+
+
+ROOT_DIR = pathlib.Path('./')
+DATA_DIR = ROOT_DIR / 'data'
+
+DICTIONARY_DATA = 'dictionary-data.txt'
+
+
 def main():
-    input_file_path = './data/dictionary-data.txt'
+    input_file_path = DATA_DIR / DICTIONARY_DATA
 
     with open(input_file_path, 'r', encoding='utf-8') as f:
         s = f.read()
